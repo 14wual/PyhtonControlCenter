8a08f3cca7e14545b96e9b07b2a4d43c4441a36f
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-26 22:17:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,20 @@ setup(
     name='PythonControlCenter',
     version='2.0.0',
     description='Python Control Center is a control center for linux & windows with GUI designed for "bspwm" environments where we dont find the classic xorg or gnome cc as in common graphical environments',
+    url='https://github.com/14wual/PyhtonControlCenter',
+    author='Carlos Padilla Labella',
+    author_email='cpadlab@gmail.com',
+    license='GPL',
     install_requires=[
         'customtkinter',
-        'PIL.Image',
+        'pillow',
         'psutil',
-        'tkinter',
         'wmi',
-        'subprocess'
-        'win32com.client',
-        'platform',
         'screen_brightness_control',
-        'ctypes',
-        'os'
         'requests'
     ],
+    project_urls={
+        'Bug Reports': 'https://github.com/14wual/PyhtonControlCenter/issues',
+        'Source': 'https://github.com/14wual/PyhtonControlCenter/blob/main/__init__.py',
+    },
 )
